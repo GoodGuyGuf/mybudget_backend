0fd71ac45540ff920434f3b638d8878d65912363
Ethan <ethan_gustafson@outlook.com>
2020-03-10 13:48:49 -0400

    able to view all expenses inside of Test expenses fetch. added one more expense seed to verify

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 
 budget_one = Budget.create(title: "March, 2020", bank: 3000)
 expense_one = Expense.create(name: "School loan", cost: "250", date: "March 30th", budget_id: 1)
+expense_two = Expense.create(name: "Car Payment", cost: "300", date: "March 28th", budget_id: 1)
