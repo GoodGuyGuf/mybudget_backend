efe7236ffb33c87cd3e40004192f6325bd2ec333
Ethan <ethan_gustafson@outlook.com>
2020-03-11 11:54:48 -0400

    expenses POST fetch functional. expenseForm eventlistener works on form

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,7 +1,10 @@
 class ExpensesController < ApplicationController
 
     def create
-        expense = Budget.expenses.build(params.require(:expense).permit(:name, :cost, :date, :budget_id))
+        budget = Budget.find_by_id(params[:budget_id])
+        expense = budget.expenses.build(params.require(:expense).permit(:name, :cost, :date, :budget_id))
+        expense.save
+        render json: expense
     end
     
 end
