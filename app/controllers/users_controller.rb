0cf6c67100e40d4469b040610bcc2038ff20501b
Ethan <ethan_gustafson@outlook.com>
2020-03-13 09:51:59 -0400

    added users show page, added users show action logic for logging in.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,20 @@ class ExpensesController < ApplicationController
         render json: user
     end
 
+    def show
+        user = User.find_by_id(params[:id])
+
+        if user
+            render json: user
+        else 
+            render json: "No User Found."
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
