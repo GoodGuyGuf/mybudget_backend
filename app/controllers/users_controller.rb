656e81ba24c7141e55f1aaf3af5b1044f9c2e81e
Ethan <ethan_gustafson@outlook.com>
2020-03-14 09:42:12 -0400

    added users index, logginginForm event listener can now send nonfunctional fetch requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,22 @@
-class ExpensesController < ApplicationController
+class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+    end
 
     def create 
-        user = User.create(user_params)
-        render json: user
+        # byebug
+        # user = User.find_by(username: params[:user][:username])
+        # if user.try(:authenticate, params[:user][:password])
+        #     render json: "Success"
+        # else 
+        #     render json: "No User Found."
+        # end
     end
 
     def show
-        user = User.find_by_id(params[:id])
-
-        if user
-            render json: user
-        else 
-            render json: "No User Found."
-        end
+        
     end
 
     private
