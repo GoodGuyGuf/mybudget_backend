1b11d5705ac26ccb62243006dbd7ac62e67d014e
Ethan <ethan_gustafson@outlook.com>
2020-03-15 12:44:50 -0400

    all relationships between classes set up. Can call Budget.all[0].expenses and get its expenses, etc.

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,9 @@
 class ExpensesController < ApplicationController
 
+    def index
+        expenses = Expense.all
+        render json: ExpenseSerializer.new(expenses)
+    end
     def create
         budget = Budget.find_by_id(params[:budget_id])
         expense = budget.expenses.build(params.require(:expense).permit(:name, :cost, :date, :budget_id))
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :budgets, only: [:index, :new, :create, :show]
-  resources :expenses, only: [:create]
+  resources :expenses, only: [:index, :create]
   resources :users, only: [:index, :create, :show]
 
   post 'login', to: 'users#session'
