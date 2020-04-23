d5d96b97e09c1d41aa62d5e8f896c2549d082048
Ethan <ethan_gustafson@outlook.com>
2020-04-23 00:05:19 -0400

    added simple validations to expense controller/model

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -7,8 +7,11 @@ class ExpensesController < ApplicationController
     def create
         budget = Budget.find_by_id(params[:budget_id])
         expense = budget.expenses.build(params.require(:expense).permit(:name, :cost, :date, :budget_id, :user_id))
-        expense.save
-        render json: expense
+        if expense.save
+            render json: ExpenseSerializer.new(expense)
+        else
+            render json: {message: "Not Accepted"}
+        end
     end
     
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,5 @@
 class Expense < ActiveRecord::Base
+    validates :name, :cost, :date, presence: true
     belongs_to :budget
     belongs_to :user
 end
