a36a030bedd06446524fd4982aedd5d26c0f046e
Ethan <ethan_gustafson@outlook.com>
2020-04-23 00:11:09 -0400

    A new expense instance will send budget remaining balance json attribute

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,8 @@ class Expense < ActiveRecord::Base
     validates :name, :cost, :date, presence: true
     belongs_to :budget
     belongs_to :user
+
+    def budget_remaining_balance
+        self.budget.remaining_balance
+    end
 end
--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -1,6 +1,6 @@
 class ExpenseSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :cost, :date, :budget_id, :user_id
+  attributes :name, :cost, :date, :budget_id, :user_id, :budget_remaining_balance
   belongs_to :budget
   belongs_to :user
 end
