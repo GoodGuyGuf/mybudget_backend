1de6648b82b235c9cec0341509323a2979e09efa
Ethan <ethan_gustafson@outlook.com>
2020-04-22 23:35:42 -0400

    added new budget methods to json serializer

--- app/serializers/budget_serializer.rb
+++ app/serializers/budget_serializer.rb
@@ -1,6 +1,6 @@
 class BudgetSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :bank, :user_id
+  attributes :title, :bank, :user_id, :expenses_total, :remaining_balance
   has_many :expenses
   belongs_to :user
 end
