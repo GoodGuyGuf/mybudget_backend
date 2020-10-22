f088cd41b8de04713dbafde7a47d5b4a704ac8ac
Ethan <ethan_gustafson@outlook.com>
2020-10-22 00:11:13 -0400

    add spec/models/user_spec with layout of tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe "ActiveModel validations" do
+
+  end
+
+  # describe "ActiveRecord associations" do
+    
+  # end
+
+  # describe "callbacks" do
+
+  # end
+
+  # describe "public instance methods" do
+
+  # end
+
+  # describe "public class methods" do
+
+  # end
+  
+end
